ad4b87bb0ce2e948978948774f4df09df8b1112d
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-02-11 17:22:39 +0700

    modify id long to integer

--- src/main/java/com/hubt/ecommerce/model/Category.java
+++ src/main/java/com/hubt/ecommerce/model/Category.java
@@ -16,7 +16,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Integer id;
 
     @Column(name = "category_name")
     private String categoryName;
---
+++ src/main/java/com/hubt/ecommerce/service/CategoryService.java
@@ -0,0 +1,2 @@
+package com.hubt.ecommerce.service;public class CategoryService {
+}
