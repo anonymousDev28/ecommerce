a9183a4e39590fee62e408dac4d174ab33148a99
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-02-11 17:25:19 +0700

    add Category service

--- src/main/java/com/hubt/ecommerce/service/CategoryService.java
+++ src/main/java/com/hubt/ecommerce/service/CategoryService.java
@@ -1,2 +1,40 @@
-package com.hubt.ecommerce.service;public class CategoryService {
+package com.hubt.ecommerce.service;
+
+import com.hubt.ecommerce.model.Category;
+import com.hubt.ecommerce.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+    public Category getCategoryById(Integer id){
+        return categoryRepository.findById(id).orElse(null);
+    }
+    public List<Category> getCategories(){
+        return categoryRepository.findAll();
+    }
+    public Category saveCategory(Category category){
+        return categoryRepository.save(category);
+    }
+    public Category updateCategory(Category category){
+        if(categoryRepository.findById(category.getId()).isPresent()){
+            Category existCategory = categoryRepository.findById(category.getId()).orElse(null);
+            existCategory.setCategoryName(category.getCategoryName());
+            existCategory.setProducts(category.getProducts());
+            categoryRepository.save(existCategory);
+            return existCategory;
+        }else {
+            return null;
+        }
+    }
+    public String deleteCategory(Integer id){
+        if(categoryRepository.findById(id).isPresent()){
+            categoryRepository.deleteById(id);
+            return "deleted category with id: "+id;
+        }else {
+            return "id "+id+" dont exist";
+        }
+    }
 }
