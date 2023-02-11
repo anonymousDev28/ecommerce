f1993ab12a4d00536243aa0d15f14682ce9628fd
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-02-11 16:57:12 +0700

    add model category

--- src/main/java/com/hubt/ecommerce/model/Category.java
+++ src/main/java/com/hubt/ecommerce/model/Category.java
@@ -1,2 +1,26 @@
-package com.hubt.ecommerce.model;public class Category {
+package com.hubt.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
 }
