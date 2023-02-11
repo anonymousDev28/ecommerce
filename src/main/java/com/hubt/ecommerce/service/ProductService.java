a541aa72343fcbf3aaebddb13cbcbe20ff7ea1e0
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-02-11 15:38:32 +0700

    add update and delete function

--- src/main/java/com/hubt/ecommerce/service/ProductService.java
+++ src/main/java/com/hubt/ecommerce/service/ProductService.java
@@ -18,4 +18,26 @@ public class ProductService {
     public Product saveProduct(Product product){
         return productRepository.save(product);
     }
+    public Product getProductById(Integer id){
+        return productRepository.findById(id).orElse(null);
+    }
+    public Product updateProduct(Product product){
+        Product existProduct = getProductById(product.getId());
+        if(existProduct!=null){
+            existProduct.setName(product.getName());
+            existProduct.setPrice(product.getPrice());
+            productRepository.save(existProduct);
+            return existProduct;
+        }else{
+            return null;
+        }
+    }
+    public String deleteProductById(Integer id){
+        if(productRepository.findById(id)!=null){
+            productRepository.deleteById(id);
+            return "deleted Product have id: "+id;
+        }else {
+            return "id invalid or dont exist";
+        }
+    }
 }
